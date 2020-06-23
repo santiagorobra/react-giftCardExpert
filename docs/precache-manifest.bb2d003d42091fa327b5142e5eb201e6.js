4e56888db5fb72e14402c158e1369cd8c4f50a8d
Santiago Robra <santiagorobra@gmail.com>
2020-06-22 21:36:50 -0300

    rename folder build to docs

---
+++ docs/precache-manifest.bb2d003d42091fa327b5142e5eb201e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302a2e571543e78093cd702804b347fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11896b7f3d8be7ab330c",
+    "url": "/static/css/main.0322e634.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11896b7f3d8be7ab330c",
+    "url": "/static/js/main.83d8a8f2.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
